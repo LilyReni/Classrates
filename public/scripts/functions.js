d32b0c714c9c376d0a34b0917f2f2a1a0ea2de3e
LilyReni <irisren7799@gmail.com>
2018-04-23 20:09:22 -0700

    api

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -1,7 +1,7 @@
 function submitComment(commentControl) {
     var comments = document.getElementsByName(commentControl)[0].value;
-    var subscriptionKey = "TEXT_ANALYTICS_API_KEY";
-    var url = "TEXT_ANALYTICS_ENDPOINT/sentiment";
+    var subscriptionKey = "0af354df86eb496a89f24043517d3c56";
+    var url = "https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment";
 
     var payload = '{ "documents": [ { "language": "en-US", "id": "1", "text": "' + comments + '" }]}';
 
